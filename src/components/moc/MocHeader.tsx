9c127dbc70032c029ad5456086a67dfe6decdedb
Manisha7477 <vermamanisha6200@gmail.com>
2025-10-22 14:55:22 +0530

    code clean

---
+++ src/components/moc/MocHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MocHeader() {
+  return (
+    <div>
+      This is the MocHeader component.
+    </div>
+  )
+}
+
+export default MocHeader
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -16,21 +16,3 @@ const Home: React.FunctionComponent<IHomeProps> = ({}) => {
 }
 
 export default Home
-
-// import { useEffect } from "react"
-// import { useNavigate } from "react-router-dom"
-
-// interface IHomeProps {}
-
-// const Home: React.FunctionComponent<IHomeProps> = ({}) => {
-//   const navigate = useNavigate()
-//   console.log("Home component rendered")
-//   useEffect(() => {
-//     console.log("Coming to Home component")
-//     navigate("/energy-monitoring/my-dashboard")
-//   }, [])
-
-//   return <>{/* <h1>HEllo world </h1> */}</>
-// }
-
-// export default Home
